11d3ddaf60a037e5a1cbfad8a4523c6e1eb9628c
Benedict Sotalbo <bsotalbo014@gmail.com>
2026-04-07 23:59:14 +0800

    Fix the db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool(process.env.DB_URL);
 
-pool.getConnection().then(conn => {
-  conn.query("SET time_zone = '+08:00'");
-  conn.release();
-});
+(async () => {
+  try {
+    const conn = await pool.getConnection();
+    await conn.query("SET time_zone = '+08:00'");
+    conn.release();
+    console.log(" MySQL timezone set to +08:00");
+  } catch (err) {
+    console.error(" Failed to set timezone:", err);
+  }
+})();
 
-module.exports = pool.promise();
+module.exports = pool;
